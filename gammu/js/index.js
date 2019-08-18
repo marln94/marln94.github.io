4bf25b75bf1466eb19f3afdaec242f5632d5674d
Marlon Calderón <mrln9eca@gmail.com>
2019-08-18 02:47:44 -0600

    Añadido Gammu

---
+++ gammu/js/index.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    
+    $('.slider').slider({
+      indicators: false,
+      duration: 1000
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
